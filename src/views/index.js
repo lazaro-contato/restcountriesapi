a51b3b7099cda6b236c6a585d3af68977c0da35c
jlazaro <contato.jlazaro@gmail.com>
2021-07-01 22:44:10 -0300

    adding the logic and finishing the css

---
+++ src/views/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".drop-data").click(function(){
+        $(".dropdown ul").toggleClass("active");
+    })
+
+    $(".drop-data").click(function(){
+        $(".dropdown ul li").click(function(){
+            var text = $(this).text();
+            $(".default-option").text(text);
+            $(".dropdown ul").removeClass("active");
+        });
+    })
+})
+
