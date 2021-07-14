fbd62cda01a9e472a349b92d98448d3a7d7a1181
jlazaro <contato.jlazaro@gmail.com>
2021-07-13 23:24:47 -0300

    adding the card of countries

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js"
-import dotenv from 'dotenv/config'
-
-const localPort = process.env.PORT || 8080;
-
-app.listen(localPort, (req, res) => {
-    console.log(`Server is running at localhost:${localPort}`)
-})
--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,6 @@
 /* Adicionando a função do filtro de continentes */
 
+
 $(document).ready(function(){
     $(".drop-data").click(function(){
         $(".dropdown ul").toggleClass("active");
@@ -12,6 +13,32 @@ $(document).ready(function(){
             $(".dropdown ul").removeClass("active");
         });
     })
+
+            fetch('https://restcountries.eu/rest/v2/all').then((response) =>{
+            response.json().then((data) => {
+                data.forEach(country => {
+                    const element = 
+                    `<section class="card-country">
+                    <div>
+                      <img src="${country.flag}" alt="country-flag"> 
+                    </div>
+                    <div class="country-info">
+                      <h3 class="country-name">${country.name}</h3>
+                      <p>Population: <span>${country.population}</span></p>
+                      <p>Region: <span>${country.region}</span></p>
+                      <p>Capital: <span>${country.capital}</span></p>
+                    </div>
+                   </section>`
+
+                   $(".card-model").append(element)
+                   console.log(response)
+                });
+            })
+        }
+        )
 })
+
+
+
 
  
