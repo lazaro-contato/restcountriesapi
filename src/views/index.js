a3d77e2a2772a0976fdfa366d78a2e942f0ae2c8
José Lázaro <contato.jlazaro@gmail.com>
2021-07-23 20:11:45 -0300

    add the filter

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,12 @@
-/* Adicionando a função do filtro de continentes */
+/* Adding a continents filter function */
 
 $(document).ready(function () {
+
+  // variable of actives countries //
+  let activesCountries = [];
+
+
+  // Filter of regions //
   $(".drop-data").click(function () {
     $(".dropdown ul").toggleClass("active");
   });
@@ -11,14 +17,16 @@ $(document).ready(function () {
 
         $(".default-option").text(text);
         $(".dropdown ul").removeClass("active");
-        RegionFilter(text)
         
+        RegionFilter(text)
+
         if (text == "All"){
           var countryCards = $(".card-country");
           for(var i = 0; i < countryCards.length; i++){
-            countryCards[i].style.display = ""
+            activesCountries = $(".card-country");
+            countryCards[i].style.display = "";
         }
-        }
+      }
     });
   });
 
@@ -55,7 +63,7 @@ $(document).ready(function () {
 
           // Defining the variables //
            let currentCountry = dataCountry[0]
-           let singleCountryFlag = currentCountry.flag
+           let singleCountryFlag = currentCountry.flag;
            let singleCountryName = currentCountry.name;
            let singleCountryNativeName = currentCountry.nativeName;
            let singleCountryPopulation = currentCountry.population;
@@ -67,72 +75,69 @@ $(document).ready(function () {
            // Adding the currencies of a country in a string //
            let arrayCurrencies = []
            currentCountry.currencies.forEach((currencies) => {
-             let countryCurrency = currencies.name
-             arrayCurrencies.push(countryCurrency)
-             let stringCurrencies = arrayCurrencies.toString().replaceAll(",", ", ")
-             localStorage.setItem('currencies', `${stringCurrencies}`)
+             let countryCurrency = currencies.name;
+             arrayCurrencies.push(countryCurrency);
+             let stringCurrencies = arrayCurrencies.toString().replaceAll(",", ", ");
+             localStorage.setItem('currencies', `${stringCurrencies}`);
            });
 
 
            // Adding the languages of a country in a string //
            let arrayLanguages = []
            currentCountry.languages.forEach((languages) => {
-            let countryLanguage = languages.name
-            arrayLanguages.push(countryLanguage)
-            let stringLanguages = arrayLanguages.toString().replaceAll(",", ", ")
-            localStorage.setItem('languages', `${stringLanguages}`)
+            let countryLanguage = languages.name;
+            arrayLanguages.push(countryLanguage);
+            let stringLanguages = arrayLanguages.toString().replaceAll(",", ", ");
+            localStorage.setItem('languages', `${stringLanguages}`);
            })
 
            // Adding the border countries in a string //
            let arrayBorderCountries = []
            currentCountry.borders.forEach((borders) => {
-             let countryBorders = borders
-             arrayBorderCountries.push(countryBorders)
-             let stringBorders = arrayBorderCountries.toString().replaceAll(",", ", ")
-             localStorage.setItem('borderCountries', `${stringBorders}`)
+             let countryBorders = borders;
+             arrayBorderCountries.push(countryBorders);
+             let stringBorders = arrayBorderCountries.toString().replaceAll(",", ", ");
+             localStorage.setItem('borderCountries', `${stringBorders}`);
            })
 
 
            // Adding the values of API to local storage //
-           localStorage.setItem('flag', `${singleCountryFlag}`)
-           localStorage.setItem('name', `${singleCountryName}`)
-           localStorage.setItem('nativeName', `${singleCountryNativeName}`)
-           localStorage.setItem('population', `${singleCountryPopulation}`)
-           localStorage.setItem('region', `${singleCountryRegion}`)
-           localStorage.setItem('subregion', `${singleCountrySubRegion}`)
-           localStorage.setItem('capital', `${singleCountryCapital}`)
-           localStorage.setItem('topLevelDomain', `${singleCountryTopLevelDomain}`)
+           localStorage.setItem('flag', `${singleCountryFlag}`);
+           localStorage.setItem('name', `${singleCountryName}`);
+           localStorage.setItem('nativeName', `${singleCountryNativeName}`);
+           localStorage.setItem('population', `${singleCountryPopulation}`);
+           localStorage.setItem('region', `${singleCountryRegion}`);
+           localStorage.setItem('subregion', `${singleCountrySubRegion}`);
+           localStorage.setItem('capital', `${singleCountryCapital}`);
+           localStorage.setItem('topLevelDomain', `${singleCountryTopLevelDomain}`);
 
-           window.location = "country.html"
+           window.location = "country.html";
 
-           console.log(dataCountry)
-           console.log(dataCountry[0].borders)
          })   
         })
       });
 
         // Selecting the elements in country.html and assigning the text of the local storage //
-         $(".first-country-flag").attr("src", `${localStorage.getItem('flag')}`)
-         $(".mainCountryName").text(`${localStorage.getItem('name')}`)
-         $(".native-name-text").text(`${localStorage.getItem('nativeName')}`)
-         $(".population-text").text(`${localStorage.getItem('population')}`)
-         $(".region-text").text(`${localStorage.getItem('region')}`)
-         $(".sub-region-text").text(`${localStorage.getItem('subregion')}`)
-         $(".capital-text").text(`${localStorage.getItem('capital')}`)
-         $(".top-level-domain-text").text(`${localStorage.getItem('topLevelDomain')}`)
-         $(".currencies-text").text(`${localStorage.getItem('currencies')}`)
-         $(".language-text").text(`${localStorage.getItem('languages')}`)
-         $(".borders-text").text(`${localStorage.getItem('borderCountries')}`)
+         $(".first-country-flag").attr("src", `${localStorage.getItem('flag')}`);
+         $(".mainCountryName").text(`${localStorage.getItem('name')}`);
+         $(".native-name-text").text(`${localStorage.getItem('nativeName')}`);
+         $(".population-text").text(`${localStorage.getItem('population')}`);
+         $(".region-text").text(`${localStorage.getItem('region')}`);
+         $(".sub-region-text").text(`${localStorage.getItem('subregion')}`);
+         $(".capital-text").text(`${localStorage.getItem('capital')}`);
+         $(".top-level-domain-text").text(`${localStorage.getItem('topLevelDomain')}`);
+         $(".currencies-text").text(`${localStorage.getItem('currencies')}`);
+         $(".language-text").text(`${localStorage.getItem('languages')}`);
+         $(".borders-text").text(`${localStorage.getItem('borderCountries')}`);
     });
   });
-  
 
   $(".back-button").click(() => {
-    window.location = "index.html"
+    window.location = "index.html";
   })
 
   $(".separator-header h3").click(() => {
-    window.location = "index.html"
+    window.location = "index.html";
   })
 
   // Adding the filter to the input
@@ -141,13 +146,13 @@ $(".input-box").keyup(() => {
   var input = $("#input-name-country");
   var inputData = input[0];
   var filter = inputData.value.toUpperCase();
-  var cards = $(".card-country");
+  var cards = activesCountries;
 
   for (i = 0; i < cards.length; i++){
     a = cards[i].getElementsByTagName("h3")[0]
     textCountry = a.textContent || a.innetText; 
     if(textCountry.toUpperCase().indexOf(filter) > -1){
-      cards[i].style.display = "block"
+      cards[i].style.display = "block";
     } else {
       cards[i].style.display = "none";
     }
@@ -159,18 +164,22 @@ $(".input-box").keyup(() => {
 function RegionFilter(region) {
   var regionFilterName = region.toUpperCase();
   var country = $(".card-country");
+  var arrayActivesRegion = [];
 
   for (i = 0; i < country.length; i++){
-    a = country[i].getElementsByClassName("regionCountryCard")[0]
+    a = country[i].getElementsByClassName("regionCountryCard")[0];
     textCountry = a.textContent || a.innetText; 
 
       if(textCountry.toUpperCase().indexOf(regionFilterName) > -1){
         country[i].style.display = "";
+        arrayActivesRegion.push(country[i]);
       } else {
         country[i].style.display = "none";
       } 
-  }   
-  
+  }  
+  activesCountries = arrayActivesRegion;
 }
+
+
 
 })
